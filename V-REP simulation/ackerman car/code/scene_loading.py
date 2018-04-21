ec0ca829f4bd22178361cd52ce422af096b54741
DaryaLari <daryalari@gmail.com>
2018-04-21 11:44:10 +0300

    Tests generation generalized

--- V-REP simulation/ackerman car/code/scene_loading.py
+++ V-REP simulation/ackerman car/code/scene_loading.py
@@ -15,8 +15,46 @@ import LineFollower
 import TestDataGenerator
 
 
+def export_data(params):
+    with open("results.csv", "a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerows(params)
+
+
+def init_connection_scene(path):
+    print('Program started')
+    ### Connect to remote API server ###
+    vrep.simxFinish(-1) # just in case, close all opened connections
+    client_id = vrep.simxStart('127.0.0.1', 19997, True, True, 5000, 5)
+    if client_id == -1:
+        print('Failed connecting to remote API server')
+        exit(-1)
+    print('Connected to remote API server')
+    ###-###
+    ### Load scene ###
+    err_code = vrep.simxLoadScene(client_id, path, 0xFF, vrep.simx_opmode_blocking)
+    if err_code != vrep.simx_return_ok:
+        print("Failed loading scene")
+        exit(-2)
+    ###-###
+    return client_id
+
+
+def close_connection_scene(client_id):
+    ### Close scene ###
+    err_code = -1
+    while err_code != vrep.simx_return_ok:
+        err_code = vrep.simxCloseScene(client_id, vrep.simx_opmode_blocking)
+        time.sleep(1)
+    ###-###
+    ### Disconnect from remote API server ###
+    # Now close the connection to V-REP:
+    vrep.simxFinish(client_id)
+    print('Program ended')
+    ###-###
+
+
 def run_test(client_id, speed, kp, ki, kd):
-    print("run test with " + str(speed) + " " + str(kp) + " " + str(ki) + " " + str(kd))
     ### Init & start simulation ###
     lf = LineFollower.LineFollower(client_id, speed, kp, ki, kd)
     err_code = vrep.simxStartSimulation(client_id, vrep.simx_opmode_oneshot)
@@ -30,6 +68,7 @@ def run_test(client_id, speed, kp, ki, kd):
     lf.runCar()
     sim_time = time.time() - start_time
     ###-###
+    export_data([[speed, kp, ki, kd, sim_time]])
 
     ### Stop simulation ###
     err_code = -1
@@ -37,67 +76,37 @@ def run_test(client_id, speed, kp, ki, kd):
         err_code = vrep.simxStopSimulation(client_id, vrep.simx_opmode_oneshot)
         time.sleep(1)
     ###-###
-
     return sim_time
 
 
-def export_data(params):
-    with open("results.csv", "w", newline="") as file:
-        writer = csv.writer(file)
-        writer.writerows([["speed", "kp", "ki", "kd", "time"]])
-        writer.writerows(params)
-
-
-def init():
-    path = "..\\ackerman_car.ttt"
-    print('Program started')
-    ### Connect to remote API server ###
-    vrep.simxFinish(-1) # just in case, close all opened connections
-    client_id = vrep.simxStart('127.0.0.1', 19997, True, True, 5000, 5)
-
-    if client_id == -1:
-        print('Failed connecting to remote API server')
-        exit(-1)
-    print('Connected to remote API server')
-    ###-###
-
-    ### Load scene ###
-    err_code = vrep.simxLoadScene(client_id, path, 0xFF, vrep.simx_opmode_blocking)
-    if err_code != vrep.simx_return_ok:
-        print("Failed loading scene")
-        exit(-2)
-    ###-###
-
-    ### Run generated test cases ###
-
-    params = [] # [0] - speed, [1] - kp, [2] - ki, [3] - kd, [4] - sim_time
-    # run_test(client_id, speed=100, kp=10, ki=0, kd=0)
-    test_gen = TestDataGenerator.TestDataGenerator()
+def run_test_engine(client_id):
+    # params = [] # [0] - speed, [1] - kp, [2] - ki, [3] - kd, [4] - sim_time
+    test_gen = TestDataGenerator.TestDataGenerator(
+        [ # [start_value, end_value, step_size]
+            [10, 100, 5], # speed
+            [10, 100, 5], # kp
+            [0, 5, 0.5], # ki
+            [0, 25, 5] # kd
+        ])
     while test_gen.has_next():
         [t_speed, t_kp, t_ki, t_kd] = test_gen.get_test_case()
         t_time = run_test(client_id, t_speed, t_kp, t_ki, t_kd)
+        # params.append([t_speed, t_kp, t_ki, t_kd, t_time])
 
-        params.append([t_speed, t_kp, t_ki, t_kd, t_time])
-    ###-###
 
-    ### Export test results to file ###
-    export_data(params)
-    ###-###
+def run_simple_test(client_id):
+    run_test(client_id, speed=100, kp=10, ki=0.1, kd=1)
 
-    ### Close scene ###
-    err_code = -1
-    while err_code != vrep.simx_return_ok:
-        err_code = vrep.simxCloseScene(client_id, vrep.simx_opmode_blocking)
-        time.sleep(1)
 
-    ###-###
+def run_program():
+    client_id = init_connection_scene(path="..\\ackerman_car.ttt") # path to scene
 
-    ### Disconnect from remote API server ###
-    # Now close the connection to V-REP:
-    vrep.simxFinish(client_id)
-    print('Program ended')
-    ###-###
+    # export_data([["speed", "kp", "ki", "kd", "time"]])
+    # run_simple_test(client_id)
+    run_test_engine(client_id)
+
+    close_connection_scene(client_id)
 
 ### Starting point ###
-init()
+run_program()
 ###-###
