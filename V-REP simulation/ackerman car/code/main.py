50a313c8f48ac79c88e953744bfd13877d05d584
DaryaLari <daryalari@gmail.com>
2018-05-04 13:55:55 +0300

    Some structural changes

---
+++ V-REP simulation/ackerman car/code/main.py
@@ -0,0 +1,45 @@
+import csv
+import scene_loading
+import TestDataGenerator
+
+
+def export_data(params):
+    with open("tests_results/results.csv", "a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerows(params)
+
+
+def run_test_engine(client_id):
+    # params = [] # [0] - speed, [1] - kp, [2] - ki, [3] - kd, [4] - sim_time
+    test_gen = TestDataGenerator.TestDataGenerator(#
+        [ # [start_value, end_value, step_size]
+            [80, 150, 5], # speed
+            [3, 10, 1], # kp
+            [0.1, 0.1, 0.1], # ki
+            [1, 5, 1] # kd
+        ])
+    while test_gen.has_next():
+        [t_speed, t_kp, t_ki, t_kd] = test_gen.get_test_case()
+        t_time = scene_loading.run_test(client_id, t_speed, t_kp, t_ki, t_kd)
+        export_data([[t_speed, t_kp, t_ki, t_kd, t_time]])
+        # params.append([t_speed, t_kp, t_ki, t_kd, t_time])
+
+
+def run_simple_test(client_id):
+    scene_loading.run_test(client_id, speed=100, kp=5, ki=0.1, kd=1)
+
+
+def run_program():
+    client_id = scene_loading.init_connection_scene(path="..\\ackerman_car_with_additional_paths.ttt") # path to scene
+
+    # export_data([["speed", "kp", "ki", "kd", "time"]])
+    # run_test_engine(client_id)
+    run_simple_test(client_id)
+
+    scene_loading.close_connection_scene(client_id)
+
+### Starting point ###
+
+
+run_program()
+###-###
--- V-REP simulation/ackerman car/code/scene_loading.py
+++ V-REP simulation/ackerman car/code/scene_loading.py
@@ -10,15 +10,7 @@ except:
     print ('')
 
 import time
-import csv
 import LineFollower
-import TestDataGenerator
-
-
-def export_data(params):
-    with open("results.csv", "a", newline="") as file:
-        writer = csv.writer(file)
-        writer.writerows(params)
 
 
 def init_connection_scene(path):
@@ -68,7 +60,6 @@ def run_test(client_id, speed, kp, ki, kd):
     lf.runCar()
     sim_time = time.time() - start_time
     ###-###
-    export_data([[speed, kp, ki, kd, sim_time]])
 
     ### Stop simulation ###
     err_code = -1
@@ -76,37 +67,5 @@ def run_test(client_id, speed, kp, ki, kd):
         err_code = vrep.simxStopSimulation(client_id, vrep.simx_opmode_oneshot)
         time.sleep(1)
     ###-###
+    print(sim_time)
     return sim_time
-
-
-def run_test_engine(client_id):
-    # params = [] # [0] - speed, [1] - kp, [2] - ki, [3] - kd, [4] - sim_time
-    test_gen = TestDataGenerator.TestDataGenerator(
-        [ # [start_value, end_value, step_size]
-            [10, 100, 5], # speed
-            [10, 100, 5], # kp
-            [0, 5, 0.5], # ki
-            [0, 25, 5] # kd
-        ])
-    while test_gen.has_next():
-        [t_speed, t_kp, t_ki, t_kd] = test_gen.get_test_case()
-        t_time = run_test(client_id, t_speed, t_kp, t_ki, t_kd)
-        # params.append([t_speed, t_kp, t_ki, t_kd, t_time])
-
-
-def run_simple_test(client_id):
-    run_test(client_id, speed=100, kp=10, ki=0.1, kd=1)
-
-
-def run_program():
-    client_id = init_connection_scene(path="..\\ackerman_car.ttt") # path to scene
-
-    # export_data([["speed", "kp", "ki", "kd", "time"]])
-    # run_simple_test(client_id)
-    run_test_engine(client_id)
-
-    close_connection_scene(client_id)
-
-### Starting point ###
-run_program()
-###-###
