cb766b4746cc03a1cb54e0b983204bc0a430b7d3
DaryaLari <daryalari@gmail.com>
2018-05-04 13:56:56 +0300

    Changed way of checking stability

--- V-REP simulation/ackerman car/code/LineFollower.py
+++ V-REP simulation/ackerman car/code/LineFollower.py
@@ -125,6 +125,12 @@ class LineFollower:
             vrep.simx_opmode_blocking)
         if err_code != vrep.simx_return_ok:
             print("Can not find right sensor")
+        err_code, self.middle_sensor = vrep.simxGetObjectHandle(
+            self.clientID,
+            'middleSensor',
+            vrep.simx_opmode_blocking)
+        if err_code != vrep.simx_return_ok:
+            print("Can not find middle sensor")
 
     def __init__(self, client_id, speed, kp, ki, kd):
         self.clientID = client_id
@@ -139,21 +145,21 @@ class LineFollower:
         self.init_back_motors()
         self.init_sensors()
 
-        # get car
+        # # get car
         # err_code, self.car = vrep.simxGetObjectHandle(
         #     self.clientID,
         #     'modAckermannSteeringCar',
         #     vrep.simx_opmode_oneshot_wait)
         # if err_code != vrep.simx_return_ok:
         #     print("Can not find car")
-        # get path
-        err_code, self.path = vrep.simxGetObjectHandle(
-            self.clientID,
-            'Path',
-            vrep.simx_opmode_oneshot_wait)
-        if err_code != vrep.simx_return_ok:
-            print("Can not find path")
-        ### - ###
+        # # get path
+        # err_code, self.path = vrep.simxGetObjectHandle(
+        #     self.clientID,
+        #     'Path',
+        #     vrep.simx_opmode_oneshot_wait)
+        # if err_code != vrep.simx_return_ok:
+        #     print("Can not find path")
+        # ### - ###
 
 
     def runCar(self):
@@ -168,7 +174,16 @@ class LineFollower:
 
         sensor_data_l = 0.5
         sensor_data_r = 0.5
+        sensor_data_m = 0.0
 
+        err_code = -1
+        while err_code != vrep.simx_return_ok:
+            err_code, state, data_middle = vrep.simxReadVisionSensor(
+            self.clientID,
+            self.left_sensor,
+            vrep.simx_opmode_blocking)
+            if err_code != vrep.simx_return_ok:
+                print("Error init middle sensor")
         err_code = -1
         while err_code != vrep.simx_return_ok:
             err_code, state, data_right = vrep.simxReadVisionSensor(
@@ -205,28 +220,28 @@ class LineFollower:
             if err_code != vrep.simx_return_ok:
                 print("Error setting base speed to right motor")
 
-        car_on_floor = True
-        while car_on_floor:
+        car_on_line = True
+        while car_on_line:
             cycle_start_time = time.time()
 
             result_r, state, data_right = vrep.simxReadVisionSensor(
                 self.clientID,
                 self.right_sensor,
                 vrep.simx_opmode_streaming)
-            if err_code != vrep.simx_return_ok:
+            if result_r != vrep.simx_return_ok:
                 print("Old data of right vision sensor are used")
 
             result_l, state, data_left = vrep.simxReadVisionSensor(
                 self.clientID,
                 self.left_sensor,
                 vrep.simx_opmode_streaming)
-            if err_code != vrep.simx_return_ok:
+            if result_l != vrep.simx_return_ok:
                 print("Old data of left vision sensor are used")
 
             if result_l == vrep.simx_return_ok:
-                sensor_data_l = data_left[0][11]
+                sensor_data_l = data_left[0][10]
             if result_r == vrep.simx_return_ok:
-                sensor_data_r = data_right[0][11]
+                sensor_data_r = data_right[0][10]
             error = sensor_data_r - sensor_data_l
             integral = integral + error
             derivative = error - prev_error
@@ -266,13 +281,46 @@ class LineFollower:
             # print(time.time() - cycle_start_time)
             time.sleep(cycleTime - (time.time() - cycle_start_time))
 
-            err_code = -1
-            while err_code != vrep.simx_return_ok:
-                err_code, position = vrep.simxGetObjectPosition(
-                    self.clientID,
-                    self.path,
-                    self.left_motor,
-                    vrep.simx_opmode_blocking)
-            # print(position)
-            if position[0] > 0.2:
-                car_on_floor = False
+            err_code, state, data_middle = vrep.simxReadVisionSensor(
+                self.clientID,
+                self.left_sensor,
+                vrep.simx_opmode_streaming)
+
+            if err_code == vrep.simx_return_ok:
+                # sensor_data_l = data_middle[0][12]
+                # _0.append(data_middle[0][0])
+                # _1.append(data_middle[0][1])
+                # _2.append(data_middle[0][2])
+                # _3.append(data_middle[0][3])
+                # _4.append(data_middle[0][4])
+                # _5.append(data_middle[0][5])
+                # _6.append(data_middle[0][6])
+                # _7.append(data_middle[0][7])
+                # _8.append(data_middle[0][8])
+                # _9.append(data_middle[0][9])
+                # _10.append(data_middle[0][10])
+                # _11.append(data_middle[0][11])
+                # _12.append(data_middle[0][12])
+                # _13.append(data_middle[0][13])
+                # _14.append(data_middle[0][14])
+                # _t.append(_t[-1] + cycleTime)
+                #
+                # plt.plot(_t, _0, color=(0, 0, 0))
+                # plt.plot(_t, _1, color=(0, 0, 0.5))
+                # plt.plot(_t, _2, color=(0, 0, 1))
+                # plt.plot(_t, _3, color=(0, 0.5, 0))
+                # plt.plot(_t, _4, color=(0, 0.5, 0.5))
+                # plt.plot(_t, _5, color=(0, 0.5, 1))
+                # plt.plot(_t, _6, color=(0, 1, 0))
+                # plt.plot(_t, _7, color=(0, 1, 0.5))
+                # plt.plot(_t, _8, color=(0, 1, 1))
+                # plt.plot(_t, _12, color=(1, 0, 0))
+                # plt.plot(_t, _13, color=(1, 0, 0.5))
+                # plt.plot(_t, _14, color=(1, 0, 1))
+                # plt.plot(_t, _9, color=(1, 0.5, 0))
+                # plt.plot(_t, _10, color=(1, 0.5, 0.5))
+                # plt.plot(_t, _11, color=(1, 0.5, 1))
+
+                # plt.figure(1).draw()
+                print(data_middle[0][11])
+                car_on_line = (data_middle[0][11] < 0.9)
